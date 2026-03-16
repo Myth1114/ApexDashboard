af0502ea2c0d1ffadd77b30fbad1595dfed0997a
mythlesh <mythlesh114@gmail.com>
2026-03-16 16:25:13 +0545

    added backend

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://asdtoaircrhbykoaczpq.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzZHRvYWlyY3JoYnlrb2FjenBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NzY5MTMsImV4cCI6MjA4OTE1MjkxM30.1IhefPda8j1UjbrJtA9iIDVEVZEmJfIkQS9acxTBC0Y";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
